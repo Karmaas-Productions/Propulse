1de1ff0ca8a9ffbf6d360b17f603c0e3455f30b6
Luke Gustafson <bugattiguy527@gmail.com>
2023-09-02 11:23:28 -0500

    Propulse TV3

--- Assets/Scripts and Assets/Drones/DroneController/Scripts/MobileControls.cs
+++ Assets/Scripts and Assets/Drones/DroneController/Scripts/MobileControls.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
-public class MobileControls : MonoBehaviour
+public class ButtonEmulator : MonoBehaviour
 {
-    public GameObject buttonsContainer; // Assign the container for the buttons in the Inspector
+    public GameObject androidIOSContainer; // Container for buttons on Android and iOS
     public Button wButton;
     public Button aButton;
     public Button sButton;
@@ -14,89 +15,93 @@ public class MobileControls : MonoBehaviour
     public Button kButton;
     public Button lButton;
 
-    private bool wButtonPressed;
-    private bool aButtonPressed;
-    private bool sButtonPressed;
-    private bool dButtonPressed;
-    private bool iButtonPressed;
-    private bool jButtonPressed;
-    private bool kButtonPressed;
-    private bool lButtonPressed;
+    private Dictionary<KeyCode, bool> keyStates = new Dictionary<KeyCode, bool>();
 
     private void Start()
     {
-        // Enable or disable the button container based on platform
-        buttonsContainer.SetActive(IsMobilePlatform());
+        // Initialize the key states dictionary
+        keyStates[KeyCode.W] = false;
+        keyStates[KeyCode.A] = false;
+        keyStates[KeyCode.S] = false;
+        keyStates[KeyCode.D] = false;
+        keyStates[KeyCode.I] = false;
+        keyStates[KeyCode.J] = false;
+        keyStates[KeyCode.K] = false;
+        keyStates[KeyCode.L] = false;
+
+        // Show/hide buttons based on platform
+#if UNITY_ANDROID || UNITY_IOS
+        androidIOSContainer.SetActive(true);
+#else
+        androidIOSContainer.SetActive(false);
+#endif
 
         // Add onClick listeners to the buttons to simulate keyboard input when buttons are clicked
-        AddClickEvent(wButton, () => StartKeyPress(KeyCode.W, () => wButtonPressed = true));
-        AddClickEvent(aButton, () => StartKeyPress(KeyCode.A, () => aButtonPressed = true));
-        AddClickEvent(sButton, () => StartKeyPress(KeyCode.S, () => sButtonPressed = true));
-        AddClickEvent(dButton, () => StartKeyPress(KeyCode.D, () => dButtonPressed = true));
-        AddClickEvent(iButton, () => StartKeyPress(KeyCode.I, () => iButtonPressed = true));
-        AddClickEvent(jButton, () => StartKeyPress(KeyCode.J, () => jButtonPressed = true));
-        AddClickEvent(kButton, () => StartKeyPress(KeyCode.K, () => kButtonPressed = true));
-        AddClickEvent(lButton, () => StartKeyPress(KeyCode.L, () => lButtonPressed = true));
+        AddButtonEvent(wButton, KeyCode.W);
+        AddButtonEvent(aButton, KeyCode.A);
+        AddButtonEvent(sButton, KeyCode.S);
+        AddButtonEvent(dButton, KeyCode.D);
+        AddButtonEvent(iButton, KeyCode.I);
+        AddButtonEvent(jButton, KeyCode.J);
+        AddButtonEvent(kButton, KeyCode.K);
+        AddButtonEvent(lButton, KeyCode.L);
     }
 
-    private bool IsMobilePlatform()
+    private void Update()
     {
-        // Check if the current platform is Android or iOS
-        return Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
-    }
-
-    private void StartKeyPress(KeyCode key, System.Action keyDownAction)
-    {
-        keyDownAction.Invoke();
-        InvokeRepeating("SimulateKeyPress", 0f, 0.1f); // Call SimulateKeyPress every 0.1 seconds while button is held
-    }
-
-    private void StopKeyPress(System.Action keyUpAction)
-    {
-        keyUpAction.Invoke();
-        CancelInvoke("SimulateKeyPress"); // Stop calling SimulateKeyPress
-    }
-
-    private void SimulateKeyPress()
-    {
-        // Simulate key press based on button states
-        if (wButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.W);
-        if (aButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.A);
-        if (sButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.S);
-        if (dButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.D);
-        if (iButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.I);
-        if (jButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.J);
-        if (kButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.K);
-        if (lButtonPressed) InputSimulator.SimulateKeyPress(KeyCode.L);
-    }
-
-    private void AddClickEvent(Button button, System.Action clickAction)
-    {
-        // Create an event trigger component if it doesn't exist
-        EventTrigger eventTrigger = button.gameObject.GetComponent<EventTrigger>();
-        if (eventTrigger == null)
+        // Simulate key presses based on button states
+        foreach (var kvp in keyStates)
         {
-            eventTrigger = button.gameObject.AddComponent<EventTrigger>();
+            if (kvp.Value)
+            {
+                SimulateKeyPress(kvp.Key);
+            }
         }
+    }
 
-        // Create an entry for PointerDown event
+    private void AddButtonEvent(Button button, KeyCode key)
+    {
+        button.onClick.AddListener(() => ToggleKeyState(key));
+        EventTrigger eventTrigger = button.gameObject.AddComponent<EventTrigger>();
+
+        // Add event trigger for pointer down (button press)
         EventTrigger.Entry pointerDownEntry = new EventTrigger.Entry();
         pointerDownEntry.eventID = EventTriggerType.PointerDown;
-        pointerDownEntry.callback.AddListener((eventData) => clickAction.Invoke());
-
-        // Add the entry to the event trigger component
+        pointerDownEntry.callback.AddListener((eventData) => ToggleKeyState(key, true));
         eventTrigger.triggers.Add(pointerDownEntry);
+
+        // Add event trigger for pointer up (button release)
+        EventTrigger.Entry pointerUpEntry = new EventTrigger.Entry();
+        pointerUpEntry.eventID = EventTriggerType.PointerUp;
+        pointerUpEntry.callback.AddListener((eventData) => ToggleKeyState(key, false));
+        eventTrigger.triggers.Add(pointerUpEntry);
+    }
+
+    private void ToggleKeyState(KeyCode key, bool state = true)
+    {
+        keyStates[key] = state;
+    }
+
+    private void SimulateKeyPress(KeyCode key)
+    {
+        // Simulate key press by setting the key as pressed
+        InputSimulator.SimulateKeyDown(key);
+        InputSimulator.SimulateKeyUp(key);
     }
 }
 
 public static class InputSimulator
 {
-    // Simulate a key press by setting the desired key as pressed
-    public static void SimulateKeyPress(KeyCode key)
+    // Simulate key down by setting the key as pressed
+    public static void SimulateKeyDown(KeyCode key)
     {
-        // Set the key as pressed
         Input.GetKeyDown(key);
         Input.GetKey(key);
+    }
+
+    // Simulate key up by setting the key as released
+    public static void SimulateKeyUp(KeyCode key)
+    {
         Input.GetKeyUp(key);
     }
 }
